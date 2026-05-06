5681c8f4c12afe53db5fb0e08b349b46ffddd86e
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2026-05-06 20:32:58 -0300

    imports delete

--- backend/src/main/java/com/albertsilva/dev/dscatalog/web/controllers/CategoryController.java
+++ backend/src/main/java/com/albertsilva/dev/dscatalog/web/controllers/CategoryController.java
@@ -6,8 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
