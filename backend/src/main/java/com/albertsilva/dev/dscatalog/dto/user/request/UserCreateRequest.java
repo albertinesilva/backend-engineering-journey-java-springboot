c78d4841479988ba751fcd981d7a9a9683cbf5d1
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2026-05-13 16:31:03 -0300

    UserCreateRequest

--- backend/src/main/java/com/albertsilva/dev/dscatalog/dto/user/request/UserCreateRequest.java
+++ backend/src/main/java/com/albertsilva/dev/dscatalog/dto/user/request/UserCreateRequest.java
@@ -2,9 +2,15 @@ package com.albertsilva.dev.dscatalog.dto.user.request;
 
 import java.util.Set;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record UserCreateRequest(
+        @NotBlank(message = "Campo obrigatório")
         String firstName,
         String lastName,
+
+        @Email(message = "Favor informar um email válido")
         String email,
         String password,
         Set<Long> roleIds) {
